19abb594ddfd909dff4fb052d3c7cb86166b2b06
wajahat <wajahatlisyed@gmail.com>
2025-07-10 20:51:10 +0500

    Student Class

---
+++ src/Student.java
@@ -0,0 +1,9 @@
+public class Student extends person {
+    public void talk(){
+        System.out.println("Student talk");
+    }
+    public void  walk(){
+        System.out.println("Student walk");
+    }
+
+}
